5415036ecd8de07a5242e9f10db64952008bfc57
Luz Fernández Acuña <luz.fernandezac@gmail.com>
2023-08-18 08:58:17 -0500

    se abrega base 1 de sistema de preguntas

---
+++ Assets/Scripts/QuestionSystem/QuizController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class QuizController : MonoBehaviour
+{
+    public float timeBetweenPrints = 7.0f;
+    public List<Question> questions = new List<Question>();
+
+    private Question lastPrinted = null; // Guarda la última cadena impresa
+
+    public TextMeshProUGUI questionText;
+    public TextMeshProUGUI[] answerTexts;
+
+    Question randomQuestion = null;
+    private void Start()
+    {
+        questions.Clear();
+        questions.AddRange(GetComponent<QuestionsReader>().questions);
+        StartCoroutine(PrintRandomTextWithDelay());
+    }
+
+    private IEnumerator PrintRandomTextWithDelay()
+    {
+        while (true)
+        {
+            randomQuestion = GetRandomQuestion();
+            char option = 'A';
+            if (randomQuestion != lastPrinted)
+            {
+                Debug.Log(randomQuestion.questionText);
+
+                questionText.text = randomQuestion.questionText;
+
+                for (int i = 0; i < randomQuestion.answerOptions.Count; i++)
+                {
+                    option++;
+                    answerTexts[i].text = option + " " + randomQuestion.answerOptions[i].answerText;
+                }
+
+                lastPrinted = randomQuestion;
+            }
+
+            yield return new WaitForSeconds(timeBetweenPrints);
+        }
+    }
+
+    private Question GetRandomQuestion()
+    {
+        Question random = lastPrinted;
+        while (random == lastPrinted)
+        {
+            random = questions[Random.Range(0, questions.Count)];
+        }
+        return random;
+    }
+
+    public void OnAnswerSelected(int answerIndex)
+    {
+        Debug.Log($"Respuesta ");
+    }
+}
