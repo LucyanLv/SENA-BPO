ecd1c57507c2f9d69f60bcd757fae5c8cffb43e1
LozanoJ10 <judalo1128@gmail.com>
2023-09-18 23:19:39 -0500

    Sistema de dialogo y acomodar npcs

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject Text1;
+    public GameObject Text2;
+    public GameObject Text3;
+    public GameObject Text4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
