a4f2d3a596b61b2c4d8e1a900d5b7a152a62afd4
LozanoJ10 <judalo1128@gmail.com>
2023-09-24 16:57:39 -0500

    80% terminado (creo)

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
     public Player_Mov player;
-    public GameObject text1;
+    public GameObject text1,launch,questionEmon;
     public GameObject text2;
     public GameObject text3;
-    public GameObject text4,text5,text6;
+    public GameObject text4,text5,text6,text7,text8,text9,text10,text11;
     public GameObject panel;
     public GameObject indic1;
     public GameObject flecha,circulo;
@@ -33,18 +33,18 @@ public class Tutorial : MonoBehaviour
     public void texto2()
     {
         text1.SetActive(false);
-        text2.SetActive(true);
-    }
+        text2.SetActive(true);}
+
     public void texto3()
     {
         text2.SetActive(false);
-        text3.SetActive(true);
-    }
+        text3.SetActive(true);}
+
     public void texto4()
     {
         text3.SetActive(false);
-        text4.SetActive(true);
-    }
+        text4.SetActive(true);}
+
     public void run()
     {
         text4.SetActive(false);
@@ -54,13 +54,55 @@ public class Tutorial : MonoBehaviour
         circulo.SetActive(true);
         player.canMove=true;
     }
+    public void run2()
+    { 
+        panel.SetActive(false);
+        player.canMove=true;
+        StartCoroutine(time());}
+        
+        
     public void texto5()
     {
         panel.SetActive(true);
         text5.SetActive(true);
         flecha.SetActive(false);
         circulo.SetActive(false);
-        player.canMove=false;
+        indic1.SetActive(false);
+        player.canMove=false;}
 
-    }
+    public void texto6()
+    {
+        text5.SetActive(false);
+        text6.SetActive(true); }
+    public void texto7()
+    {
+        text6.SetActive(false);
+        text7.SetActive(true);}
+
+    public void texto8()
+    {
+        text7.SetActive(false);
+        text8.SetActive(true);}
+    
+    public void texto9()
+    {
+        text8.SetActive(false);
+        text9.SetActive(true);}
+
+    public void texto10()
+    {
+        text9.SetActive(false);
+        text10.SetActive(true);}
+
+        public void texto11()
+    {
+        text10.SetActive(false);
+        text11.SetActive(true);}
+        
+        IEnumerator time()
+        {
+            yield return new WaitForSeconds(2f);
+            questionEmon.SetActive(true);
+            launch.SetActive(true);
+        }
 }
