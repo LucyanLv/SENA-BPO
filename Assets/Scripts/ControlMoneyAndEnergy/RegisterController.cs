54da1dd08053696f106f8136df99c4768a47916c
Luz Fernández Acuña <luz.fernandezac@gmail.com>
2023-10-08 19:35:00 -0500

    revision login/register para usuarios

--- Assets/Scripts/ControlMoneyAndEnergy/RegisterController.cs
+++ Assets/Scripts/ControlMoneyAndEnergy/RegisterController.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 using System.Net;
 using System;
 
-public class Register : MonoBehaviour
+public class RegisterController : MonoBehaviour
 {
     public InputField UsernameInput;
     public InputField PasswordInput;
@@ -23,20 +23,17 @@ public class Register : MonoBehaviour
         Gotologinbutton.onClick.AddListener(goToLoginScene);
         if (File.Exists(Application.dataPath + "/ credentials.txt *"))
         {
-
             credentials = new ArrayList(File.ReadAllLines(Application.dataPath + "/ credentials.txt *"));
         }
 
         else
         {
             File.WriteAllText(Application.dataPath + "/ credentials.txt ", "");
-
         }
     }
 
     void goToLoginScene()
     {
-
         SceneManager.LoadScene("Login");
     }
 
@@ -44,44 +41,26 @@ public class Register : MonoBehaviour
     void writeStufflofile()
     {
         bool isExists = false;
-        credentials = new ArrayList(File.ReadAllLines(Application.dataPath+ "/credentials.txt"));
+        credentials = new ArrayList(File.ReadAllLines(Application.dataPath + "/credentials.txt"));
+        Debug.Log(Application.dataPath + "/credentials.txt");
         foreach (var i in credentials)
         {
             if (i.ToString().Contains(UsernameInput.text))
             {
                 isExists = true;
                 break;
-
             }
-
         }
         if (isExists)
         {
-
             Debug.Log($"Usernam  ' {UsernameInput.text}' already exists");
-
-
-
-
         }
         else
         {
-
             credentials.Add(UsernameInput.text + ":" + PasswordInput.text);
             File.WriteAllLines(Application.dataPath + "/ credentials.txt", (String[])credentials.ToArray(typeof(string)));
-          Debug.Log("Account Registered");
-
+            Debug.Log("Account Registered");
         }
     }
-   
-        
-   
-
-    
 
 }
-
-
-
-
-
